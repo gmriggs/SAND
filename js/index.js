b9f4a425289ad3f94346b549418a61d2b1cf8dff
ItsASine <ItsASine@users.noreply.github.com>
2015-12-28 20:05:56 -0500

    Add thing for rockerchu.8306 :kissing_heart:

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var easter_egg = new Konami();
+
+easter_egg.code = function () {
+    $(function () {
+        $("#dialog").fadeIn("slow")
+            .dialog({
+                width: 550,
+                modal: true,
+                buttons: {
+                    Ok: function () {
+                        $(this).dialog("close");
+                    }
+                }
+            });
+    });
+};
+
+easter_egg.load();
